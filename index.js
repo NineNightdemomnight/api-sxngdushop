3d5527e255dbb7594f15343eb88905c3fd988601
NineNightdemomnight <42994065+NineNightdemomnight@users.noreply.github.com>
2024-09-21 21:43:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
+// index.js
 const express = require('express')
-const byFluxus = require('./api/byfluxus.js')
-const byRelzScript = require('./api/byrelzscript.js')
 
-/*import express from 'express';
-import byFluxus from './api/byfluxus.js';
-import byRelzScript from './api/byrelzscript.js';*/
+const app = express()
+const PORT = 4000
 
-const app = express();
-const PORT = process.env.PORT || 4000;
-
-// เส้นทางหลัก
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳');
-});
-
-// เส้นทางเกี่ยวกับ
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ');
-});
-
-// ใช้งาน API
-app.use('/api/byfluxus', byFluxus);
-app.use('/api/byrelzscript', byRelzScript);
-
-// เริ่มต้นเซิร์ฟเวอร์
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT}`);
-});
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
 // Export the Express API
-export default app;
+module.exports = app
